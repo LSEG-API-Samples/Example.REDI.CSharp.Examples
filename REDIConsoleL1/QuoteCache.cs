d4003cacae5f03616ad143246122abb9df621a58
Zoya Farberov <zoya.farberov@thomsonreuters.com>
2019-01-24 17:42:43 -0500

    Add files via upload

---
+++ REDIConsoleL1/QuoteCache.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using RediLib;
+using System.Collections.Specialized;
+
+namespace RediConsoleL1
+{
+    class QuoteCache //: INotifyPropertyChanged
+    {
+        private string _symbol;
+
+        public string Symbol
+        {
+            get { return _symbol; }
+            set { this._symbol =  value; }
+        }
+        private readonly CacheControl quoteCache;
+        private object err = null;
+
+
+        //public event PropertyChangedEventHandler PropertyChanged;
+
+        public QuoteCache(CacheControl qc, string Symbol)
+        {
+            quoteCache = qc;
+            _symbol = Symbol;
+        }
+
+        public enum CacheControlActions
+        {
+            Snapshot = 1,
+            Add = 4,
+            Update = 5,
+            Delete = 8
+        }
+
+        public void Subscribe()
+        {
+            if (Symbol != null)
+            {
+                err = null;
+                if (Symbol.Contains(' '))
+                {
+                    quoteCache.CacheEvent += quoteCacheHandler;
+                    quoteCache.AddWatch(WatchType.L1OPT, Symbol, null, ref err);
+                    quoteCache.Submit("L1", Symbol, ref err);
+                }
+                else
+                {
+                    quoteCache.CacheEvent += quoteCacheHandler;
+                    quoteCache.AddWatch(WatchType.L1, Symbol, null, ref err);
+                    quoteCache.Submit("L1", "", ref err);
+                }
+            }
+
+        }
+
+
+
+
+
+
+        public enum WatchType
+        {
+            L1,
+            L2,
+            PANDL,
+            FIRM,
+            L1OPT,
+            TimesAndSales,
+            HistoricalTimesAndSales
+        }
+
+        public static object GetCell(CacheControl cc, int row, string columnName, out int errorCode)
+        {
+            object value = null;
+            object errCode = null;
+            cc.GetCell(row, columnName, ref value, ref errCode);
+            errorCode = (int)errCode;
+            if (value != null)
+            {
+                return value;
+            }
+            return string.Empty;
+        }
+
+        private void quoteCacheHandler(int action, int row)
+        {
+            string Symbol;
+            string Bid;
+            string Ask;
+            string Last;
+            int errCode;
+
+ 
+            if (row >= 0)
+            {
+
+                switch (action)
+                {
+                    case (int)CacheControlActions.Add:
+                    case (int)CacheControlActions.Update:
+                    case (int)CacheControlActions.Snapshot:
+                        try
+                        {
+                            if (action == (int)CacheControlActions.Snapshot)
+                                row = 0;
+
+
+
+                            Symbol = GetCell(quoteCache, row, "Symbol", out errCode).ToString().TrimStart();
+                            if (Symbol.Length == 0)
+                            {
+                                Symbol = GetCell(quoteCache, row, "DisplaySymbol", out errCode).ToString().TrimStart();
+                            }
+
+ //                           Console.WriteLine("Symbol=" + Symbol + " Action=" + ((CacheControlActions)action).ToString()); // + " Row=" + row.ToString());
+
+
+                            Bid = "";
+                            Ask = "";
+
+
+                            if (Symbol.Contains(' '))
+                            {
+                                Bid = GetCell(quoteCache, row, "BidPrice", out errCode).ToString();
+                                Ask = GetCell(quoteCache, row, "AskPrice", out errCode).ToString();
+                            }
+                            else
+                            {
+                                Bid = GetCell(quoteCache, row, "Bid", out errCode).ToString();
+                                Ask = GetCell(quoteCache, row, "Ask", out errCode).ToString();
+                            }
+
+                            Last = GetCell(quoteCache, row, "Last", out errCode).ToString();
+
+                            /* var quote = _qw.Quotes.FirstOrDefault(X => X.Symbol == Symbol);
+
+                             if (quote != null)
+                             {
+                                 quote.Bid = Bid;
+                                 quote.Ask = Ask;
+                                 quote.Last = Last;
+
+                             } */
+                            Console.WriteLine("Symbol=" + Symbol + " Action=" + ((CacheControlActions)action).ToString()+ 
+                                " Bid="+Bid + " Ask="+Ask+" Last="+Last);
+
+
+                        }
+                        catch
+                        {
+                        }
+                        break;
+
+                }
+            }
+        }
+        
+    
+    }
+}
---
+++ REDIConsoleL1/RediConsoleL1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using RediLib;
+
+
+namespace RediConsoleL1
+{
+    class DisplayRediL1Quotes
+    {
+
+        private RediLib.CacheControl quoteCacheControl;
+
+        
+        public enum CacheControlActions
+        {
+            Snapshot = 1,
+            Add = 4,
+            Update = 5,
+            Delete = 8
+        }
+
+        public enum WatchType
+        {
+            L1,
+            L2,
+            PANDL,
+            FIRM,
+            L1OPT,
+            TimesAndSales,
+            HistoricalTimesAndSales
+        }
+
+        Dictionary<string, QuoteCache> QuotesDict = new Dictionary<string, QuoteCache>();
+        List<string> myRICList = new List<string>(new string[] { "GOOG", "BA" ,"MSFT"});
+
+        public bool Init()
+        {
+            try
+            {    
+                foreach (string i in myRICList)
+                {
+                    if (!QuotesDict.ContainsKey(i))
+                    {
+                        quoteCacheControl = new CacheControl();
+                        QuoteCache qCache = new QuoteCache(quoteCacheControl, i);
+                        qCache.Subscribe();
+                        QuotesDict.Add(i, qCache);
+                    }
+                } 
+                return true;
+            }
+            catch (System.Runtime.InteropServices.COMException come)
+            {
+                Console.WriteLine("\nException <<< IS REDIPlus RUNNING? >>>\n\n" + come);
+                Console.WriteLine("Press any key to continue . . .");
+                Console.ReadLine();
+                return false;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("...Start of RediConsoleL1...");
+            DisplayRediL1Quotes myL1 = new DisplayRediL1Quotes();
+            if (myL1.Init())
+            {
+                Console.WriteLine("...Init completed...");
+
+               while (true)
+                {  //
+                }
+            }
+        }
+    }
+    
+}
+
